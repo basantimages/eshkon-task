67cbe3736e3d46dd1c136d946080890994e1cc04
Basantraj Shakti <basantrajshakti@gmail.com>
2023-08-19 21:17:17 +0530

    authOptions code separated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
-import { NextAuthOptions } from 'next-auth';
+import { authOptions } from '@/utils/utils';
 import NextAuth from 'next-auth/next';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.SECRET,
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,16 @@
 import { createClient } from 'contentful';
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.SECRET,
+};
 
 const client = createClient({
   space: process.env.CONTENTFUL_SECRET as string,
