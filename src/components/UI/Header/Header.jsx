19b21313350ae32d19784f75f43fd01596245a3f
Basantraj Shakti <basantrajshakti@gmail.com>
2023-08-19 20:51:57 +0530

    end changed to flex-end in header

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,12 +1,13 @@
 'use client';
 
-import styles from './Header.module.css';
 import { signOut } from 'next-auth/react';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <button
+        className={styles['logout-btn']}
         onClick={() => {
           signOut();
         }}
